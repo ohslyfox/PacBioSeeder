c8312aa3d68efdc33feff68fe6ceca44273dda99
Robert 'Max' Oller <46455756+MaxOller@users.noreply.github.com>
2020-04-28 21:49:19 -0700

    Added solution and scheme comparison

--- main/PacBioSeeder.cpp
+++ main/PacBioSeeder.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include "Loader.h"
-#include "Analyzer.h"
 #include "HashMapClusteringScheme.h"
 
 int main()
@@ -17,13 +16,17 @@ int main()
 	Loader loader;
 	auto referenceGenome = loader.LoadRawTextFile(".\\testsim\\testsim.txt");
 	auto pacBioReads = loader.LoadFastQReads(".\\testsim\\testsim_0001.fastq", 4);
+	auto solutionIndexes = loader.LoadSolutions(".\\testsim\\testsim_0001.maf", 4);
 	HashMapClusteringScheme* scheme = new HashMapClusteringScheme(referenceGenome, 9);
 
+	vector<int> indexes;
 	for (int i = 0; i < 4; i++) {
 		auto res = scheme->ExecuteScheme(pacBioReads[i], 4);
-		for (auto location : res) {
-			cout << location << ", ";
+		for (int j = 0; j < 4; j++) {
+			cout << "Scheme result: " << res[0] << endl;
+			cout << "Solution: " << solutionIndexes[j] << endl;
+			cout << "Difference: " << solutionIndexes[j] - res[0] << endl << endl;
 		}
-		cout << endl << endl;
+		cout << endl << endl << endl;
 	}
 }
